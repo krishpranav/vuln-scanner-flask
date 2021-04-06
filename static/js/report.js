eba9f928dc566be6682eafae97feaf52d80430a9
krishpranav <krisna.pranav@gmail.com>
2021-04-06 12:36:31 +0530

    adding files

---
+++ static/js/report.js
@@ -0,0 +1,16 @@
+const scanDetails = document.querySelector(".scan-details");
+const payloadsTried = document.querySelector(".payloads-tried");
+const scanLogs = document.querySelectorAll(".scan-logs");
+const payloads = document.querySelectorAll(".payloads");
+const testLogs = document.querySelectorAll(".test-logs");
+const scanDetails1 = document.querySelector(".scan-details1");
+const risk = document.querySelectorAll(".risk");
+const rec = document.querySelector(".rec");
+const reco = document.querySelector(".reco");
+const recom = document.querySelectorAll(".recom");
+const rec1 = document.querySelector(".rec1");
+const reco1 = document.querySelector(".reco1");
+const recom1 = document.querySelectorAll(".recom1");
+const sqlid = document.querySelector(".sqlid");
+const xssd = document.querySelector(".xssd");
+
