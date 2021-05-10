093c71c7c3daa2b30d50efa2a1cf112106e69a29
krishpranav <krisna.pranav@gmail.com>
2021-05-10 12:12:37 +0530

    added logging

---
+++ core/logging.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# imports
+import sys
+import logging
+
+from config import LOG_LEVEL, WEB_LOG
+
+logger = logging.getLogger('VULN-SCANNER-FLASK')
+level  = logging.getLevelName(LOG_LEVEL)
+logger.setLevel(level)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(level)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(process)d - %(message)s')
+ch.setFormatter(formatter)
+
+fh = logging.FileHandler('logs/' + WEB_LOG)
+fh.setFormatter(formatter)
+fh.setLevel(level)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
